e9a45f322b7aa5300e29eb38fb92280895446979
Mel Ugaddan <ugaddanmeljohn@gmail.com>
2022-10-28 20:35:09 +0800

    Delete server/dist directory

--- server/dist/constants/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SPACE_DATA_URL = void 0;
-exports.SPACE_DATA_URL = "https://api.spacexdata.com/v2/";
--- server/dist/resolver/Query/launch.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.launch = void 0;
-const launch = (_, { id }, context) => {
-    return context.dataSources.launchAPI.getLaunchById({ launchId: id });
-};
-exports.launch = launch;
